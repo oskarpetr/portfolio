68b6581d346bc6ba5b2b1379e06452ee16973419
Oskar Petr <oskarpetr@macoskar.local>
2024-01-01 14:33:06 +0100

    add pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,23 @@
-import { ArrowRight } from "@phosphor-icons/react/dist/ssr";
 import { motion } from "framer-motion";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   const menu = ["Development", "Writing", "Projects"];
 
+  const scrolltoHash = function (element: string) {
+    document.getElementById(element)?.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest",
+    });
+  };
+
   return (
     <div>
-      <div className="text-white h-screen px-32 py-16 flex flex-col gap-24 bg-[url('/images/Background2.png')] bg-x">
+      <div className="text-white h-screen px-32 py-16 flex flex-col gap-32 transition-all duration-500 bg-left bg-[url('/images/Background.png')]">
         <Link href="/">
           <motion.h1
-            className="text-xl font-semibold uppercase tracking-wide"
+            className="text-xl uppercase tracking-wide"
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
@@ -28,9 +34,10 @@ export default function Home() {
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 1 + index * 0.25 }}
-                className="tracking-wide group"
+                className="tracking-wide group cursor-pointer"
+                onClick={() => scrolltoHash(item.toLocaleLowerCase())}
               >
-                <p className="transition-all duration-500">{item}.</p>
+                <p className="transition-all duration-500">{item}</p>
 
                 <div className="h-[2px] bg-neutral-300 rounded-full w-0 group-hover:w-full transition-all duration-500"></div>
                 {/* <div className="bg-white w-0 group-hover:w-full duration-500 h-[40px] mt-[-40px]"></div> */}
@@ -38,6 +45,18 @@ export default function Home() {
             );
           })}
         </div>
+      </div>
+
+      <div id="development" className="h-screen px-32 py-16">
+        <p className="text-white text-3xl">Development</p>
+      </div>
+
+      <div id="writing" className="h-screen px-32 py-16">
+        <p className="text-white text-3xl">Writing</p>
+      </div>
+
+      <div id="projects" className="h-screen px-32 py-16">
+        <p className="text-white text-3xl">Projects</p>
       </div>
     </div>
   );
