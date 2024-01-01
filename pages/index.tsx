eff9b57944b59d35798207f30e3d9000699157a8
Oskar Petr <oskarpetr@macoskar.local>
2024-01-01 01:32:44 +0100

    main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,42 @@
+import { ArrowRight } from "@phosphor-icons/react/dist/ssr";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="text-white">
-      <div className="px-32 py-16 flex gap-16 items-center">
-        <motion.h1
-          className="text-3xl font-medium"
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          Oskar Petr
-        </motion.h1>
+  const menu = ["Development", "Writing", "Projects"];
 
-        <div className="flex gap-8 uppercase text-neutral-300">
-          <motion.button
+  return (
+    <div>
+      <div className="text-white h-screen px-32 py-16 flex flex-col gap-24 bg-[url('/images/Background2.png')] bg-x">
+        <Link href="/">
+          <motion.h1
+            className="text-xl font-semibold uppercase tracking-wide"
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.25 }}
+            transition={{ duration: 0.5 }}
           >
-            Development
-          </motion.button>
-          <motion.button
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.5 }}
-          >
-            Writing
-          </motion.button>
-          <motion.button
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.75 }}
-          >
-            Projects
-          </motion.button>
+            Oskar Petr.
+          </motion.h1>
+        </Link>
+
+        <div className="flex flex-col items-start gap-12 text-white text-3xl">
+          {menu.map((item, index) => {
+            return (
+              <motion.button
+                key={item}
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 1 + index * 0.25 }}
+                className="tracking-wide group"
+              >
+                <p className="transition-all duration-500">{item}.</p>
+
+                <div className="h-[2px] bg-neutral-300 rounded-full w-0 group-hover:w-full transition-all duration-500"></div>
+                {/* <div className="bg-white w-0 group-hover:w-full duration-500 h-[40px] mt-[-40px]"></div> */}
+              </motion.button>
+            );
+          })}
         </div>
       </div>
     </div>
