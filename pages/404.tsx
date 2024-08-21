95d98c435241efb8c98e2a9c2ed1b00ed5e165a4
Oskar Petr <oskar.petr@outlook.com>
2024-08-21 22:07:36 +0200

    Updated 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,16 +1,20 @@
 import FadeIn from "@/components/general/FadeIn";
 import Layout from "@/components/general/Layout";
 import Menu from "@/components/general/Menu";
+import TextStagger from "@/components/general/TextStagger";
 
 export default function Custom404() {
   return (
-    <Layout title="404">
-      <Menu showMenu />
+    <Layout title="Not found">
+      <Menu />
 
-      <FadeIn className="flex flex-col justify-center items-center gap-4">
+      <TextStagger
+        className="flex flex-col justify-center items-center gap-4"
+        delay={0.5}
+      >
         <h1 className="font-bold text-5xl">Error 404</h1>
         <p className="opacity-50">Oops! This page does not exist!</p>
-      </FadeIn>
+      </TextStagger>
     </Layout>
   );
 }
