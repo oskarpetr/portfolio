07b4d478b4a7f28c56ceb7f045c968efefd6de62
petr.oskar <petr.oskar@ssakhk.cz>
2025-02-27 20:02:43 +0100

    Align left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 export default function HomePage() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
-      <div className="text-xl font-semibold">Coming soon</div>
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="mx-16 flex flex-col gap-4">
+        <div className="text-xl font-semibold">Coming soon</div>
 
-      <p className="mx-20 max-w-xl text-center leading-tight font-medium opacity-50">
-        I am a cybernetics student who loves to pursue web development, design,
-        and writing. I help individuals and businesses make a memorable online
-        presence.
-      </p>
+        <p className="max-w-xl text-left leading-tight font-medium opacity-50">
+          I am a cybernetics student who loves to pursue web development,
+          design, and writing. I help individuals and businesses make a
+          memorable online presence.
+        </p>
+      </div>
     </div>
   );
 }
