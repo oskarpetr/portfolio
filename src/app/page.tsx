6ee79814d2aafa8d16b38f442bbfe42ba816e1cd
petr.oskar <petr.oskar@ssakhk.cz>
2025-03-01 23:12:56 +0100

    Coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import ComingSoon from "@/components/layout/ComingSoon";
 import Projects from "@/components/projects/Projects";
 import { getProjects } from "@/utils/cms";
 
 export default async function HomePage() {
   const projects = await getProjects();
-
+  return <ComingSoon />;
   return <Projects projects={projects} />;
 }
