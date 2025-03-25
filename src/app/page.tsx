901b133517dc421f7dc800ca9c8716a263648ffb
petr.oskar <petr.oskar@ssakhk.cz>
2025-03-25 17:08:38 +0100

    Removed unfinished components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,19 @@ import {
   getAbout,
   getProjectsShort,
   getServices,
-  getArticles,
-  getGraphicDesigns,
-  getTestimonials,
+  // getArticles,
+  // getGraphicDesigns,
+  // getTestimonials,
 } from "@/utils/cms";
 import { rootMetadata } from "@/utils/seo";
-import { cache, Suspense } from "react";
+import { cache } from "react";
 import PageLayout from "@/components/layout/PageLayout";
 import ProjectsAboutWrapper from "@/components/wrappers/home/ProjectsAboutWrapper";
 import ServicesWrapper from "@/components/wrappers/home/ServicesWrapper";
-import ArticlesWrapper from "@/components/wrappers/home/ArticlesWrapper";
-import GraphicDesignsWrapper from "@/components/wrappers/home/GraphicDesignsWrapper";
-import TestimonialsWrapper from "@/components/wrappers/home/TestimonialsWrapper";
-import ContactWrapper from "@/components/wrappers/home/ContactWrapper";
-import EmptyPage from "@/components/layout/EmptyPage";
+// import ArticlesWrapper from "@/components/wrappers/home/ArticlesWrapper";
+// import GraphicDesignsWrapper from "@/components/wrappers/home/GraphicDesignsWrapper";
+// import TestimonialsWrapper from "@/components/wrappers/home/TestimonialsWrapper";
+// import ContactWrapper from "@/components/wrappers/home/ContactWrapper";
 
 export const revalidate = 300;
 
@@ -23,16 +22,14 @@ export const revalidate = 300;
 const fetchProjectsShort = cache(getProjectsShort);
 const fetchAbout = cache(getAbout);
 const fetchServices = cache(getServices);
-const fetchArticles = cache(getArticles);
-const fetchGraphicDesigns = cache(getGraphicDesigns);
-const fetchTestimonials = cache(getTestimonials);
+// const fetchArticles = cache(getArticles);
+// const fetchGraphicDesigns = cache(getGraphicDesigns);
+// const fetchTestimonials = cache(getTestimonials);
 
+// sections
 async function ProjectsAboutSection() {
-  const [projects, about] = await Promise.all([
-    fetchProjectsShort(),
-    fetchAbout(),
-  ]);
-
+  const projects = await fetchProjectsShort();
+  const about = await fetchAbout();
   return <ProjectsAboutWrapper projects={projects} about={about} />;
 }
 
@@ -41,45 +38,34 @@ async function ServicesSection() {
   return <ServicesWrapper services={services} />;
 }
 
-async function ArticlesSection() {
-  const articles = await fetchArticles();
-  return <ArticlesWrapper articles={articles} />;
-}
+// async function ArticlesSection() {
+//   const articles = await fetchArticles();
+//   return <ArticlesWrapper articles={articles} />;
+// }
 
-async function GraphicDesignsSection() {
-  const graphicDesigns = await fetchGraphicDesigns();
-  return <GraphicDesignsWrapper graphicDesigns={graphicDesigns} />;
-}
+// async function GraphicDesignsSection() {
+//   const graphicDesigns = await fetchGraphicDesigns();
+//   return <GraphicDesignsWrapper graphicDesigns={graphicDesigns} />;
+// }
 
-async function TestimonialsSection() {
-  const testimonials = await fetchTestimonials();
-  return <TestimonialsWrapper testimonials={testimonials} />;
-}
+// async function TestimonialsSection() {
+//   const testimonials = await fetchTestimonials();
+//   return <TestimonialsWrapper testimonials={testimonials} />;
+// }
 
-export default function HomePage() {
+// async function ContactSection() {
+//   return <ContactWrapper />;
+// }
+
+export default async function HomePage() {
   return (
     <PageLayout>
-      <Suspense fallback={<EmptyPage />}>
-        <ProjectsAboutSection />
-      </Suspense>
-
-      <Suspense fallback={<EmptyPage />}>
-        <ServicesSection />
-      </Suspense>
-
-      <Suspense fallback={<EmptyPage />}>
-        <ArticlesSection />
-      </Suspense>
-
-      <Suspense fallback={<EmptyPage />}>
-        <GraphicDesignsSection />
-      </Suspense>
-
-      <Suspense fallback={<EmptyPage />}>
-        <TestimonialsSection />
-      </Suspense>
-
-      <ContactWrapper />
+      <ProjectsAboutSection />
+      <ServicesSection />
+      {/* <ArticlesSection />
+      <GraphicDesignsSection />
+      <TestimonialsSection />
+      <ContactSection /> */}
     </PageLayout>
   );
 }
